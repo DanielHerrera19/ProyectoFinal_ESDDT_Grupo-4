728982b4670cc59d751c2c213268212a4a6d32d3
Leonardo David Cruz Quispe <N00374895@upn.pe>
2024-09-17 19:30:38 -0500

    Agregue la pantalla del menu principal

--- ProyectoFinal_ESDDT_Grupo 4/Program.cs
+++ ProyectoFinal_ESDDT_Grupo 4/Program.cs
@@ -10,7 +10,41 @@ namespace ProyectoFinal_ESDDT_Grupo_4
     {
         static void Main(string[] args)
         {
-
+            int opcion = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("  Bienvenido a clinica oftalmologica ");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("*                                   ");
+                Console.WriteLine("             ▄█████▄               ");
+                Console.WriteLine("           ███  █  ███             ");
+                Console.WriteLine("          ███   █   ███            ");
+                Console.WriteLine("           ███  █  ███             ");
+                Console.WriteLine("             ▀█████▀               ");
+                Console.WriteLine("                                   *");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("          1.Personal médico          ");
+                Console.WriteLine("          2.Usuario                  ");
+                Console.WriteLine("          3.Salir                    ");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Eliga opcion:");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        opcion = PantallasSecundarias.PersonalMedico();
+                        break;
+                    case 2:
+                        opcion = PantallasSecundarias.Usuario();
+                        break;
+                    case 0:
+                    default:
+                        Console.ReadKey();
+                        break;
+                }
+            } while (opcion != 3);
         }
     }
 }
