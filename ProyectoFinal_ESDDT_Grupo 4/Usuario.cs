c1f87f5bf02091673e273d1439bd103e524bdc00
FioCelesteRomero20 <rfiorella489@gmail.com>
2024-09-17 20:14:39 -0500

    Cree una clase

---
+++ ProyectoFinal_ESDDT_Grupo 4/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ESDDT_Grupo_4
+{
+    internal class Usuario
+    {
+        public int Número;
+        public int Mes;
+        public int Año;
+        public int Cvv;
+        public int Dni;
+        public Usuario sgte;
+
+        public Usuario(int número, int mes, int año, int cvv, int dni)
+        {
+            this.Número = número;
+            this.Mes = mes;
+            this.Año = año;
+            this.Cvv = cvv;
+            this.Dni = dni;
+            sgte = null;
+        }
+    }
+}
